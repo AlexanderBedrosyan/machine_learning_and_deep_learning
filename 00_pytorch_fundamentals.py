8b3791ee05cdead733ce97ad63216eda7d54782d
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-05-07 16:47:30 +0300

    Add file with first steps of pytorch

---
+++ 00_pytorch_fundamentals.py
@@ -0,0 +1,8 @@
+## Resource notebook: https://www.learnpytorch.io/00_pytorch_fundamentals/
+
+# Imports needed
+
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
